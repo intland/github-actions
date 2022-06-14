64af12b914fd3b9ae9eaf944d66d3fcc1f423126
Sándor Zelei <sandor.zelei@intland.com>
2022-06-14 20:58:42 +0200

    Update main.py

--- ticketlinker/main.py
+++ ticketlinker/main.py
@@ -12,7 +12,6 @@ from libs.utils import *
 log_level = os.environ.get('INPUT_LOG_LEVEL', 'INFO')
 logging.basicConfig(format='ACTION: %(message)s', level=log_level)
 
-
 def main():
     access_token = os.environ.get("INPUT_ACCESS_TOKEN")
     codebeamer_user = os.environ.get("INPUT_CODEBEAMER_USER")
@@ -26,6 +25,8 @@ def main():
 
     g = Github(access_token)
     pr = getPullRequest(g)
+    
+    wait_for_mergeable_pr(pr, 60)
     
     codebeamer_tickets = getTickets(pr, (codebeamer_user, codebeamer_password))
 
