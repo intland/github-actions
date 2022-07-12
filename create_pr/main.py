306fd9f04ede8a213fc5d637e6b0fa9184487528
MikiGergely <miklos.gergely@intland.com>
2022-07-12 11:42:04 +0200

    update create_pr/main.py

--- create_pr/main.py
+++ create_pr/main.py
@@ -13,14 +13,17 @@ def main():
     if head_branch := os.environ.get("INPUT_HEAD_BRANCH") is None:
         if head_branch := os.environ.get("HEAD_BRANCH") is None:
             raise Exception("head branch can't be determined from parameters or environment variables")
+    logging.debug(head_branch)
 
     if target_branch := os.environ.get("INPUT_TARGET_BRANCH") is None:
         if target_branch := os.environ.get("TARGET_BRANCH") is None:
             raise Exception("target branch can't be determined from parameters or environment variables")
+    logging.debug(target_branch)
 
     if repository_name := os.environ.get("INPUT_REPOSITORY_NAME") is None:
         if repository_name := os.environ.get("GITHUB_REPOSITORY") is None:
             raise Exception("repository name can't be determined from parameters or environment variables")
+    logging.debug(repository_name)
 
     access_token = os.environ.get("INPUT_ACCESS_TOKEN")
     if not access_token:
@@ -29,7 +32,7 @@ def main():
     g = Github(access_token)
     repo = g.get_repo(repository_name)
     pr = repo.create_pull(owner=repo.owner.login, repo=repo.name, base=target_branch, head=head_branch)
-    print(pr.url)
+    logging.info(pr.url)
 
 
 if __name__ == '__main__':
