484d1fe105775e53344cfc33b602412b2656bc8f
MikiGergely <mgergely@ptc.com>
2023-04-26 15:36:05 +0200

    fix test

--- test/main.py
+++ test/main.py
@@ -102,7 +102,7 @@ def main():
                 break
 
     with open(os.environ.get("GITHUB_OUTPUT"), "w") as f:
-        f.write(f'extra_parameters="{json.dumps(extra_params)}"')
+        f.write(f'"extra_parameters=\'{json.dumps(extra_params)}\'"')
 
 
 if __name__ == "__main__":
