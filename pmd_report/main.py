3ced3240a85ad00c011902e71c34f59912f6569f
Sándor Zelei <szelei@ptc.com>
2025-05-03 10:55:00 +0200

    Fixed variable name

--- pmd_report/main.py
+++ pmd_report/main.py
@@ -107,7 +107,7 @@ def get_review_comment_cache(g):
         metadata = loadMetadata(meta_data_id, review_comment)
         if metadata:
             md5_hash = metadata.get('md5Hash')
-            review_comment_map[md5Hash] = review_comment
+            review_comment_map[md5_hash] = review_comment
     
     logging.info(f'review_comment_map: {review_comment_map}')
     return review_comment_map
