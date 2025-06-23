0e9988ddc2ddf6ee72c1d8527b1d8927934d2008
RobertRadi <78905316+radirobi@users.noreply.github.com>
2025-06-23 22:57:04 +0200

    Exception handling for issue comment deletion

--- libs/utils.py
+++ libs/utils.py
@@ -212,9 +212,12 @@ def issue_comment(githubApi, metadata_id, content, metadata={}):
     content += "\n" + createMetadata(metadata_id, metadata)
     if comment:
         logging.info("Comment is deleted")
-        comment.delete()
+        try:
+            comment.delete()
+            logging.info("New comment is created")
+        except Exception as e:
+            logging.info(f'Comment can not be deleted, error code: {e.response.status_code}')
 
-    logging.info("New comment is created")
     pr.create_issue_comment(content)
 
 def delete_review_comments(github_api, user_name):
